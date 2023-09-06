b8bf648ecd1fd149c9ceebd20655cf45d96c7d01
lh-128 <59599222+lh-128@users.noreply.github.com>
2023-09-06 01:22:00 -0700

    Update signIn.py

--- mintapi/signIn.py
+++ mintapi/signIn.py
@@ -192,10 +192,10 @@ def _create_web_driver_at_mint_com(
         chrome_options.add_argument("no-sandbox")
         chrome_options.add_argument("headless")
         chrome_options.add_argument("headless=new")
-        chrome_options.add_argument("remote-debugging-port=9222")
+        chrome_options.add_argument("remote-debugging-port=9515")
         chrome_options.add_argument("disable-dev-shm-usage")
         chrome_options.add_argument("disable-gpu")
-        chrome_options.addArguments("crash-dumps-dir=/tmp")
+        chrome_options.add_arguments("crash-dumps-dir=/tmp")
         # chrome_options.add_argument("--window-size=1920x1080")
     if session_path is not None:
         chrome_options.add_argument("user-data-dir=%s" % session_path)
